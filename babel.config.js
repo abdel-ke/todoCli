257516aa70a5446848eb611a0123410f79bb9613
abdel-ke <kemmal1337@gmail.com>
2024-06-20 00:27:16 +0100

    trying to use firebase google auth

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
+api.cache(false)
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module:react-native-dotenv']
+  ]
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,66 @@
-import {Text, View} from 'react-native';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import {useEffect, useState} from 'react';
+import {Button, Text, View} from 'react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId: '',
+});
 
 const App = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  useEffect(() => {
+    auth().onAuthStateChanged(userState => {
+      setUser(userState);
+
+      if (loading) {
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  if (!user) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>Please login</Text>
+        <Button
+          title="Login"
+          onPress={() => {
+            auth()
+              .signInWithEmailAndPassword('test@test.com', '123456')
+              .then(() => {
+                console.log('User signed in');
+              })
+              .catch(error => {
+                console.error(error);
+              });
+          }}
+        />
+        <Button
+          title="login with google"
+          onPress={() => {
+            console.log(process.env.CLIENTID);
+            // auth()
+            //   .signInWithPopup(provider)
+            //   .then((result) => {
+            //     console.log('User signed in');
+            //   })
+            //   .catch(error => {
+            //     console.error(error);
+            //   });
+          }}
+        />
+      </View>
+    );
+  }
+
   return (
     <View
       style={{
@@ -8,7 +68,8 @@ const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Hello World</Text>
+      <Text>Welcome {user.email}</Text>
+      <Button title="Logout" onPress={() => auth().signOut()} />
     </View>
   );
 };
